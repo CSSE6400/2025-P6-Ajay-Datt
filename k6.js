6aa73c4838799a7ad49c27a9cdce2d489665dd55
Ajay Datt <ajaydattbusiness@gmail.com>
2025-04-10 03:03:16 +0000

    2

---
+++ k6.js
@@ -0,0 +1,15 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export const options = {
+  stages: [
+    { target: 1000, duration: '1m' },  // Ramp up to 1000 users over 1 min
+    { target: 5000, duration: '10m' }, // Hold at 5000 users for 10 min
+  ],
+};
+
+export default function () {
+  const res = http.get('http://taskoverflow-238658411.us-east-1.elb.amazonaws.com/api/v1/todos');
+  check(res, { 'status was 200': (r) => r.status === 200 });
+  sleep(1);
+}
